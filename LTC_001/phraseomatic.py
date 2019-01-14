0bb6e561478b73a26980673e6752baf3a288aadd
Keith Turner <turner@keithturner.com>
2019-01-14 06:23:44 -0700

    Adding a sync

--- LTC_001/phraseomatic.py
+++ LTC_001/phraseomatic.py
@@ -5,7 +5,7 @@ verbs = ['Leverage','Sync', 'Target','Gamify', 'Offline', 'Crowd-sourced',
          '24/7', 'Lean-in', '30,000 foot']
 
 adjectives = ['A/B Tested','Freemium', 'Hyperlocal','Siloed', 'B-to-B',
-              'Oriented', 'Cloud-based', 'API-enabled']
+              'Oriented', 'Cloud-based', 'API-enabled', 'Snergistic']
 
 nouns = ['Early-Adopter','Low-hanging fruit', 'Pipeline','Splash Page',
          'Productivity', 'Process', 'Tipping Point', 'Paradigm']
