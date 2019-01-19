c39cefc7424e83a0d1a8b645f270cb31d27ede9c
Keith Turner <turner@keithturner.com>
2019-01-18 19:25:30 -0700

    Adding dog query

--- LTC_002/dog.py
+++ LTC_002/dog.py
@@ -1,7 +1,7 @@
-input ("What is your dog's name?")
+# input ("What is your dog's name?")
 
 dog_name = input ("What is your dog's name?")
 
-input ("What is your dog's age?")
+# input ("What is your dog's age?")
 
 dog_age = input ("What is your dog's age?")
