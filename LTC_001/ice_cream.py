db07c475df88ec416adfbd738ba765da62420990
Keith Turner <turner@keithturner.com>
2019-03-10 08:09:55 -0600

    Update system

--- LTC_001/ice_cream.py
+++ LTC_001/ice_cream.py
@@ -3,7 +3,7 @@ import random
 customers = ['Luke', 'Leia', 'Darth', 'C3PO', 'R2D2', 'Lando', 'Han', 'Chewy']
 winner = random.choice(customers)
 flavor = 'vanilla'
-print('Congratulations ' + winner + ' you have won an ice cream sundae!')
+print('Congratulations, ' + winner + ' you have won an ice cream sundae!')
 prompt = 'Would you like a cherry on top (yes/no)? '
 wants_cherry = input(prompt)
 order = flavor + ' sundae '
