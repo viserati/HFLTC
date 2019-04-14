218094f16cb5f0ef35369bd873ba7c48be8377c8
Keith Turner <turner@keithturner.com>
2019-04-14 04:02:02 -0600

    Update system

--- LTC_001/ice_cream.py
+++ LTC_001/ice_cream.py
@@ -1,6 +1,6 @@
 
 import random
-customers = ['Luke', 'Leia', 'Darth', 'C3PO', 'R2D2', 'Lando', 'Han', 'Chewy', 'Kewei' ]
+customers = ['Luke', 'Leia', 'Darth', 'C3PO', 'R2D2', 'Lando', 'Han', 'Chewy', 'Kewei', 'C0D1' ]
 winner = random.choice(customers)
 flavor = 'vanilla'
 print('Congratulations, ' + winner + ' you have won an ice cream sundae from the Creamery!!!')
